473f924006f85405b1609eec04d88e912c6f80e7
Cristobal <cristobalws@gmail.com>
2023-03-06 11:52:09 -0700

    First Commid

---
+++ wallpaperapi/Controllers/DefaultController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using wallpaperapi.Models.Response;
+
+namespace wallpaperapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : Controller
+    {
+        public readonly string _version;
+        public readonly string _enviroment;
+
+
+        public DefaultController(IConfiguration configuration) {
+
+            var serverConf = configuration.GetSection("ServerConfiguration");
+            _version = serverConf["Version"];
+            _enviroment = serverConf["Enviroment"];
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return Ok(new BaseResponse { Message = "Server running on version: " + _version + ", Enviroment: " + _enviroment });
+        }
+    }
+}
---
+++ wallpaperapi/Models/Response/BaseResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace wallpaperapi.Models.Response
+{
+    public class BaseResponse
+    {
+        public int Code { get; set; } = 200;
+        public string Message { get; set; } = "";
+        public bool Error { get; set; } = false;
+
+    }
+}
